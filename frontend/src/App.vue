c11905dac050a97f76bc6b64f453f71135cfe8ec
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-10-04 15:27:03 +0300

    Configure ESLint

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -26,36 +22,20 @@
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-main> </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue';
 
 export default Vue.extend({
   name: 'App',
 
-  components: {
-    HelloWorld
-  },
-
   data: () => ({
     //
-  })
-})
+  }),
+});
 </script>
