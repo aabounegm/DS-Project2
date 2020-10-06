9aa4c112ffd93fc49d0ddf7a1f6c894afd5dd93f
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-10-06 15:30:55 +0300

    Implement managing remote URLs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,10 +9,44 @@
     </v-app-bar>
 
     <v-main>
-      <FileBrowser
-        :storages="remotes"
-        :storage="selectedRemote"
-      />
+      <v-row no-gutters>
+        <v-col cols="3">
+          <v-list>
+            <v-subheader>Remotes</v-subheader>
+            <v-list-item-group>
+              <v-list-item v-for="remote in remotes" :key="remote.url">
+                <v-list-item-icon>
+                  <v-icon>{{ remote.icon }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>{{ remote.name }}</v-list-item-title>
+                  <v-list-item-subtitle>{{ remote.url }}</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <v-btn icon @click="remove(remote.url)">
+                    <v-icon color="error">
+                      mdi-delete
+                    </v-icon>
+                  </v-btn>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+          <v-divider />
+          <div class="px-4" style="display: grid;">
+            <v-text-field v-model="name" label="Name" hint="Name of remote server" />
+            <v-text-field v-model="url" label="Address" hint="http://10.0.0.1" />
+            <v-btn fab class="ml-auto" color="pink" :dark="!!name && !!url" @click="add" :disabled="!name || !url">
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </div>
+        </v-col>
+        <v-col cols="9">
+          <FileBrowser
+            :storages="remotes"
+          />
+        </v-col>
+      </v-row>
     </v-main>
   </v-app>
 </template>
@@ -22,19 +56,6 @@ import Vue from 'vue';
 import { Remote } from '@/types';
 import FileBrowser from '@/components/FileBrowser.vue';
 
-const remotes: Remote[] = [
-  {
-    name: 'Local',
-    url: 'http://localhost:7507',
-    icon: 'mdi-folder-multiple-outline',
-  },
-  {
-    name: 'Remote 1',
-    url: 'http://', // TODO: take as input somehow
-    icon: 'mdi-cloud',
-  },
-];
-
 export default Vue.extend({
   name: 'App',
   components: {
@@ -42,9 +63,42 @@ export default Vue.extend({
   },
   data () {
     return {
-      remotes,
-      selectedRemote: remotes[0],
+      remotes: [
+        {
+          name: 'Local',
+          url: 'http://localhost:7507',
+          icon: 'mdi-folder-multiple-outline',
+        },
+      ] as Remote[],
+      name: '',
+      url: 'http://',
     };
+  },
+  methods: {
+    add () {
+      const { name, url } = this;
+      if (name === '' || url === '') {
+        return;
+      }
+      if (this.remotes.find(r => r.url === url) != null) {
+        alert('Remote already exists');
+        return;
+      }
+      this.remotes.push({
+        name,
+        url,
+        icon: 'mdi-cloud',
+      });
+      this.name = '';
+      this.url = 'http://';
+    },
+    remove (url: string) {
+      const index = this.remotes.findIndex(r => r.url === url);
+      if (index === -1) {
+        return;
+      }
+      this.remotes.splice(index, 1);
+    },
   },
 });
 </script>
