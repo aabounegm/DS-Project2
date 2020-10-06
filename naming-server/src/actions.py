9636d2038989402a3035385f049b4235bf6b6889
Lev Chelyadinov <leva181777@gmail.com>
2020-10-06 19:57:53 +0300

    Fix the f-string

--- naming-server/src/actions.py
+++ naming-server/src/actions.py
@@ -6,7 +6,7 @@ from src.extensions import mongo
 def heartbeat():
     servers = mongo.db.servers.find()
     for server in servers:
-        response = requests.get('http://{server["_id"]/heartbeat}')
+        response = requests.get(f'http://{server["_id"]}/heartbeat')
         if not response.ok:
             affected_files = mongo.db.index.find({
                 'is_directory': False,
