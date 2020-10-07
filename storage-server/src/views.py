0b560927df47bcbbf32cf938d291b5d1983eb291
Lev Chelyadinov <leva181777@gmail.com>
2020-10-07 22:14:53 +0300

    Move the server code into the structure

--- storage-server/src/views.py
+++ storage-server/src/views.py
@@ -28,7 +28,7 @@ def heartbeat():
 class FileAPI(MethodView):
     #==============================================reading file=========================
     def get(self, pathToFile: str):
-        full_path = storage_root/pathToFile 
+        full_path = storage_root/pathToFile
 
         if full_path.exists():
                 response = app.make_response(full_path.read_bytes())
@@ -36,31 +36,31 @@ class FileAPI(MethodView):
         else:
             abort(404)
 #       return jsonify({"text : ":pathToFile})
-        
+
     #==============================================create file=========================
 
-    def post0(self, pathToFile: str, fileContent):   
+    def post0(self, pathToFile: str, fileContent):
 
         folder=pathToFile[0:pathToFile.rfind('/')]
         f = open(pathToFile, "w")
         f.write(fileContent)
         f.close()
-        memory = shutil.disk_usage(folder)  
+        memory = shutil.disk_usage(folder)
         return str(memory[2])
 
-    
-    def post1(self, pathToFile: str):                          
+
+    def post1(self, pathToFile: str):
         #data = request.get_json()
         full_path = storage_root/pathToFile
         fileContent= request.files['file'].read()
-        
+
         folder=pathToFile[0:pathToFile.rfind('/')]    #to keep the path without the name of the file
         if os.path.exists(full_path):
             abort(404)
-        else:  
+        else:
             full_path.parent.mkdir(parents= True, exist_ok = True)
-            
-            
+
+
     #==============================================Update file=========================
 
     def put0(self, pathToFile, fileContent):
@@ -69,18 +69,18 @@ class FileAPI(MethodView):
         f = open(pathToFile, "a+")
         f.write(fileContent)
         f.close()
-        memory = shutil.disk_usage(folder)  
+        memory = shutil.disk_usage(folder)
         return str(memory[2])
 
 
-    def put1(self, pathToFile):                    #this is the main function to update 
+    def put1(self, pathToFile):                    #this is the main function to update
         full_path = storage_root/pathToFile
         fileContent= request.files['file'].read()
-        
+
         folder=pathToFile[0:pathToFile.rfind('/')]    #to keep the path without the name of the file
         if os.path.exists(full_path):
             abort(404)
-        else:  
+        else:
             full_path.parent.mkdir(parents= True, exist_ok = True)
 
     #==============================================Delete file=========================
@@ -88,17 +88,17 @@ class FileAPI(MethodView):
         folder=path[0:path.rfind('/')]
         if os.path.exists(path):
             os.remove(path)
-            memory = shutil.disk_usage(folder) 
+            memory = shutil.disk_usage(folder)
             if len(os.listdir(folder))==0:
                 shutil.rmtree(folder)    #to delete a folder if it's empty
                 #print("folder deleted")
-            return str(memory[2]) 
+            return str(memory[2])
         else:
             abort(404)
-        
+
 #==============================================Initialize folder=========================
-    
-@app.route('/INITIALIZE/<string:pth>', methods=['GET'])    
+
+@app.route('/INITIALIZE/<string:pth>', methods=['GET'])
 def initialize_folder(pth) :
     folder=path[0:path.rfind('\\')]
     for sub in os.listdir(pth) :
@@ -106,10 +106,8 @@ def initialize_folder(pth) :
             shutil.rmtree(folder+"\\"+sub)
         else:
             os.remove(folder+"\\"+sub)
-    memory = shutil.disk_usage(folder)  
-    return "the amount of free storage on the storage server in bytes is: "+str(memory[2]) 
-            
+    memory = shutil.disk_usage(folder)
+    return "the amount of free storage on the storage server in bytes is: "+str(memory[2])
+
 if __name__ == '__main__':
     app.run()
-
-
