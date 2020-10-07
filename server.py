54ba5a2bfd816f6b100c879b8774fd257443ee83
CeeVarouqa <ceevarouqa@gmail.com>
2020-10-07 19:42:21 +0300

    fix first 5 points

--- server.py
+++ server.py
@@ -1,99 +1,100 @@
-from flask import Flask,jsonify, request
+from flask import Flask,jsonify, request, abort
+from flask.views import MethodView
 import os
 import shutil
 import pathlib
 
 
 app = Flask(__name__)
+storage_root = pathlib.Path(os.getenv('STORAGE_DIR', '/var/storage'))
+
+
+file_api = FileAPI.as_view('file_api')
+app.add_url_rule('/file/<path:path>',
+                 view_func=file_api,
+                 methods=('GET', 'POST', 'PUT', 'DELETE'))
+app.add_url_rule('/file/',
+                 view_func=file_api,
+                 defaults={'path': ''},
+                 methods=('GET', 'POST', 'PUT', 'DELETE'))
 
 #==============================================heartbeat<3=========================
 
-@app.route('/heartbeat' , methods=['GET', 'POST'])
+@app.route('/heartbeat' , methods=['GET'])
 def heartbeat():
-    return "204"
+    return ('', 204)
 
-#==============================================reading file=========================
-@app.route('/GET/<string:pathToFile>', methods=['GET'])
-def GetFile(pathToFile):
-    if os.path.exists(pathToFile):
-        f = open(pathToFile, "r")
-        text=f.read()
-        f.close()
-        return text
-    else:
-        return "404"
+
+class FileAPI(MethodView):
+    #==============================================reading file=========================
+    def get(self, pathToFile: str):
+        full_path = storage_root/pathToFile 
+
+        if full_path.exists():
+                response = app.make_response(full_path.read_bytes())
+                return response
+        else:
+            abort(404)
 #       return jsonify({"text : ":pathToFile})
-
-    
-#==============================================create file=========================
-
-def Create_File0(pathToFile,fileContent):   #this is an auxiliary function to create a simple file 
-
-    folder=pathToFile[0:pathToFile.rfind('\\')]
-    f = open(pathToFile, "w")
-    f.write(fileContent)
-    f.close()
-    memory = shutil.disk_usage(folder)  
-    return "200  \nthe amount of free storage on the storage server in bytes is: "+str(memory[2])
-
-    
-@app.route('/POST/<string:pathToFile>', methods=['GET', 'POST'])
-def Create_File1():                         
-    data = request.get_json()
-    fileContent=data['files']
-    
-    folder=pathToFile[0:pathToFile.rfind('\\')]    #to keep the path without the name of the file
-    if os.path.exists(pathToFile):
-        return "400"
-    else:  
-        if os.path.exists(folder):
-            Create_File0(pathToFile,fileContent)
-        else:
-            #print("no existing folder")
-            os.makedirs(folder)
-            Create_File0(pathToFile,fileContent)
-          
         
-#==============================================Update file=========================
+    #==============================================create file=========================
 
-def Update_File0(pathToFile,fileContent):       #this is an auxiliary function to update a simple file with keeping the old content
-    folder=pathToFile[0:pathToFile.rfind('\\')]
-    f = open(pathToFile, "a+")
-    f.write(fileContent)
-    f.close()
-    memory = shutil.disk_usage(folder)  
-    return "200  \nthe amount of free storage on the storage server in bytes is: "+str(memory[2])
+    def post0(self, pathToFile: str, fileContent):   
 
-@app.route('/PUT/<string:pathToFile>', methods=['GET', 'POST'])
-def Update_File1():                             #this is the main function to update 
-    data = request.get_json()
-    fileContent=data['files']
+        folder=pathToFile[0:pathToFile.rfind('/')]
+        f = open(pathToFile, "w")
+        f.write(fileContent)
+        f.close()
+        memory = shutil.disk_usage(folder)  
+        return str(memory[2])
+
     
-    folder=pathToFile[0:pathToFile.rfind('\\')]
-    if os.path.exists(pathToFile):
-        Update_File0(pathToFile,fileContent)
-    else:  
-        if os.path.exists(folder):
-            Create_File0(pathToFile,fileContent)
+    def post1(self, pathToFile: str):                          
+        #data = request.get_json()
+        full_path = storage_root/pathToFile
+        fileContent= request.files['file'].read()
+        
+        folder=pathToFile[0:pathToFile.rfind('/')]    #to keep the path without the name of the file
+        if os.path.exists(full_path):
+            abort(404)
+        else:  
+            full_path.parent.mkdir(parents= True, exist_ok = True)
+            
+            
+    #==============================================Update file=========================
+
+    def put0(self, pathToFile, fileContent):
+        #this is an auxiliary function to update a simple file with keeping the old content
+        folder=pathToFile[0:pathToFile.rfind('/')]
+        f = open(pathToFile, "a+")
+        f.write(fileContent)
+        f.close()
+        memory = shutil.disk_usage(folder)  
+        return str(memory[2])
+
+
+    def put1(self, pathToFile):                    #this is the main function to update 
+        full_path = storage_root/pathToFile
+        fileContent= request.files['file'].read()
+        
+        folder=pathToFile[0:pathToFile.rfind('/')]    #to keep the path without the name of the file
+        if os.path.exists(full_path):
+            abort(404)
+        else:  
+            full_path.parent.mkdir(parents= True, exist_ok = True)
+
+    #==============================================Delete file=========================
+    def delete(self, pathToFile):
+        folder=path[0:path.rfind('/')]
+        if os.path.exists(path):
+            os.remove(path)
+            memory = shutil.disk_usage(folder) 
+            if len(os.listdir(folder))==0:
+                shutil.rmtree(folder)    #to delete a folder if it's empty
+                #print("folder deleted")
+            return str(memory[2]) 
         else:
-            #print("no existing folder")
-            os.makedirs(folder)
-            Create_File0(pathToFile,fileContent)
-
-#==============================================Delete file=========================
-
-@app.route('/DELETE/<string:path>', methods=['GET'])
-def Delete_File(path) :
-    folder=path[0:path.rfind('\\')]
-    if os.path.exists(path):
-        os.remove(path)
-        memory = shutil.disk_usage(folder) 
-        if len(os.listdir(folder))==0:
-            shutil.rmtree(folder)    #to delete a folder if it's empty
-            #print("folder deleted")
-        return "200 \nthe amount of free storage on the storage server in bytes is: "+str(memory[2]) 
-    else:
-        return "404"
+            abort(404)
         
 #==============================================Initialize folder=========================
     
